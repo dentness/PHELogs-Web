f0de406ca5aab7b0d562895752ca104614893404
Joe Dent <14243877+dentness@users.noreply.github.com>
2019-08-18 19:10:34 -0500

    WEB

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'PHE Logs' });
+  res.render('index', { title: 'PHE Logs', user: req.user });
 });
 
 module.exports = router;
--- routes/records.js
+++ routes/records.js
@@ -4,14 +4,19 @@ const RecordDataService = require('../services/record.data.service');
 
 /* GET records page. */
 router.get('/', (req, res) => {
-  res.render('records', { title: 'PHE Logs', records:  new RecordDataService().get_diet_records() });
+  new RecordDataService(req.app.locals.DATA_URL).get_diet_records().then(records => {
+    res.render('records', {title: 'PHE Logs', records: records.data});
+  });
 });
 
 /* GET record_details page. */
 router.get('/details/:id', (req, res) => {
-  const rs = new RecordDataService();
-  const record = rs.get_diet_record(req.params.id);
-  res.render('record_details', { title: 'PHE Logs', record: record, tot_board: rs.calc_tot_board(record) } );
+  const rs = new RecordDataService(req.app.locals.DATA_URL);
+  rs.get_diet_record(req.params.id).then(record => {
+    res.render('record_details', {title: 'PHE Logs', record: record.data, tot_board: rs.calc_tot_board(record.data)});
+  }).catch(reason => {
+    console.log(reason);
+  });
 });
 
 module.exports = router;
