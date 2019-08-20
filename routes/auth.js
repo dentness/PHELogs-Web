c2c46bf2bc6c0c7b574864dfcbf1e64d6eed92b4
Joe Dent <14243877+dentness@users.noreply.github.com>
2019-08-19 23:05:07 -0500

    Updated data organization for testing.

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,7 @@ router.get('/login', passport.authenticate('auth0', {
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
+  console.log("request: ", JSON.stringify(req));
   passport.authenticate('auth0', function (err, user, info) {
     if (err) { return next(err); }
     if (!user) { return res.redirect('/login'); }
@@ -24,7 +25,7 @@ router.get('/callback', function (req, res, next) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
-      res.redirect(returnTo || '/user');
+      res.redirect(returnTo || '/');
     });
   })(req, res, next);
 });
