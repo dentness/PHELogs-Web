8dec8574e58cf9b1ead64b523296ea79ac823a4f
Joe Dent <14243877+dentness@users.noreply.github.com>
2019-08-25 09:49:20 -0500

    Login cleanup

--- services/record.data.service.js
+++ services/record.data.service.js
@@ -3,23 +3,17 @@ const axios = require('axios');
 class RecordDataService {
 
   constructor() {
-    this.records = require('./static_data').getStaticRecords();
   }
 
-  get_todays_record() {
-    // return
-    return this.today;
-  }
-
-  get_diet_record(id) {
+  static get_diet_record(id) {
     return axios.get(process.env.DATA_URL + '/records/' + id);
   }
 
-  get_diet_records(from, to) {
+  static get_diet_records(from, to) {
     return axios.get(process.env.DATA_URL + '/records');
   }
 
-  calc_tot_board(record) {
+  static calc_tot_board(record) {
     var daily_total = 0.0;
     for (var i = 0; i < record.diet_record_details.length; i++) {
       daily_total += record.diet_record_details[i].phe;
@@ -30,6 +24,14 @@ class RecordDataService {
       daily_total: daily_total,
       remaining_goal: record.target_phe_level - daily_total
     };
+  }
+
+  static create_diet_record(user, date) {
+
+  }
+
+  static update_diet_record( record ) {
+
   }
 }
 
